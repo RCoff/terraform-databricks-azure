b3d27201422b8f9fa7dd68c69eb5fe2b48695091
RCoff <coffman.ridge@gmail.com>
2025-10-04 18:56:04 -0400

    Add private dns zones and virtual network links

--- networking.hub.tf
+++ networking.hub.tf
@@ -5,3 +5,39 @@ resource "azurerm_virtual_network" "hub" {
 
   address_space = ["10.0.0.0/16"]
 }
+
+resource "azurerm_private_dns_zone" "storage_account_blob" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.network.name
+}
+
+resource "azurerm_private_dns_zone" "storage_account_dfs" {
+  name                = "privatelink.dfs.core.windows.net"
+  resource_group_name = azurerm_resource_group.network.name
+}
+
+resource "azurerm_private_dns_zone" "key_vault" {
+  name                = "privatelink.vaultcore.azure.net"
+  resource_group_name = azurerm_resource_group.network.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "storage_account_blob" {
+  name                  = "link-${local.env}-${var.location_abbreviation}-storage_account_blob-01"
+  resource_group_name   = azurerm_resource_group.network.name
+  private_dns_zone_name = azurerm_private_dns_zone.storage_account_blob.name
+  virtual_network_id    = azurerm_virtual_network.hub.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "storage_account_dfs" {
+  name                  = "link-${local.env}-${var.location_abbreviation}-storage_account_dfs-01"
+  resource_group_name   = azurerm_resource_group.network.name
+  private_dns_zone_name = azurerm_private_dns_zone.storage_account_dfs.name
+  virtual_network_id    = azurerm_virtual_network.hub.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "key_vault" {
+  name                  = "link-${local.env}-${var.location_abbreviation}-key_vault-01"
+  resource_group_name   = azurerm_resource_group.network.name
+  private_dns_zone_name = azurerm_private_dns_zone.key_vault.name
+  virtual_network_id    = azurerm_virtual_network.hub.id
+}
