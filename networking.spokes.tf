1055a8de7e53a59a1928530d91bfb0c011e19df8
RCoff <coffman.ridge@gmail.com>
2025-10-22 15:24:32 -0400

    Create subnets for Databricks

--- networking.spokes.tf
+++ networking.spokes.tf
@@ -78,3 +78,65 @@ resource "azurerm_virtual_network_peering" "hub--to--spoke-databricks" {
   virtual_network_name      = azurerm_virtual_network.hub.name
   remote_virtual_network_id = azurerm_virtual_network.spoke-databricks.id
 }
+
+resource "azurerm_subnet" "databricks-private-01" {
+  name                 = "snet-${local.env}-${var.location_abbreviation}-databricks_private-01"
+  resource_group_name  = azurerm_resource_group.network.name
+  virtual_network_name = azurerm_virtual_network.spoke-databricks.name
+  address_prefixes     = ["10.2.1.0/24"]
+
+  delegation {
+    name = "databricks_private"
+
+    service_delegation {
+      name = "Microsoft.Databricks/workspaces"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [delegation[0].service_delegation[0].actions]
+  }
+}
+
+resource "azurerm_subnet" "databricks-public-01" {
+  name                 = "snet-${local.env}-${var.location_abbreviation}-databricks_public-01"
+  resource_group_name  = azurerm_resource_group.network.name
+  virtual_network_name = azurerm_virtual_network.spoke-databricks.name
+  address_prefixes     = ["10.2.2.0/24"]
+
+  delegation {
+    name = "databricks_public"
+
+    service_delegation {
+      name = "Microsoft.Databricks/workspaces"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [delegation[0].service_delegation[0].actions]
+  }
+}
+
+resource "azurerm_network_security_group" "databricks-private" {
+  name                = "nsg-${local.env}-${var.location_abbreviation}-databricks_private-01"
+  location            = azurerm_resource_group.network.location
+  resource_group_name = azurerm_resource_group.network.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "databricks-private" {
+  subnet_id                 = azurerm_subnet.databricks-private-01.id
+  network_security_group_id = azurerm_network_security_group.databricks-private.id
+}
+
+resource "azurerm_network_security_group" "databricks-public" {
+  name                = "nsg-${local.env}-${var.location_abbreviation}-databricks_public-01"
+  location            = azurerm_resource_group.network.location
+  resource_group_name = azurerm_resource_group.network.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "databricks-public" {
+  subnet_id                 = azurerm_subnet.databricks-public-01.id
+  network_security_group_id = azurerm_network_security_group.databricks-public.id
+}
+
+
